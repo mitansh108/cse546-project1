5a2f752a24c7a4788a7f13a5ae58d77cabeb212d
mitansh108 <131152918+mitansh108@users.noreply.github.com>
2025-09-27 16:29:46 -0700

    Add files via upload

---
+++ server.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, Response
+import os
+import boto3
+import io
+from concurrent.futures import ThreadPoolExecutor
+from botocore.config import Config
+
+app = Flask(__name__)
+
+config = Config(
+    max_pool_connections=100,
+    retries={'max_attempts': 1, 'mode': 'standard'},
+    region_name='us-east-1'
+)
+
+s3_client = boto3.client('s3', config=config)
+sdb_client = boto3.client('sdb', config=config)
+executor = ThreadPoolExecutor(max_workers=50)
+
+def upload_to_s3(file_content, filename):
+    try:
+        file_like = io.BytesIO(file_content)
+        s3_client.upload_fileobj(file_like, '1233383933-in-bucket', filename)
+    except:
+        pass
+
+def get_classification(item_name):
+    try:
+        response = sdb_client.get_attributes(
+            DomainName='1233383933-simpleDB',
+            ItemName=item_name,
+            AttributeNames=['classification'],
+            ConsistentRead=False
+        )
+        for attr in response.get('Attributes', []):
+            if attr['Name'] == 'classification':
+                return attr['Value']
+        return None
+    except:
+        return None
+
+@app.route('/', methods=['POST'])
+def handle_request():
+    if 'inputFile' not in request.files:
+        return Response('No inputFile provided', status=400, mimetype='text/plain')
+    
+    file = request.files['inputFile']
+    if file.filename == '':
+        return Response('No file selected', status=400, mimetype='text/plain')
+    
+    filename = file.filename
+    filename_without_ext = os.path.splitext(filename)[0]
+    
+    # Read file content once
+    file.seek(0)
+    file_content = file.read()
+    
+    # Start S3 upload in background (fire and forget)
+    executor.submit(upload_to_s3, file_content, filename)
+    
+    # Get classification immediately
+    classification = get_classification(filename_without_ext)
+    
+    if classification:
+        result = f'{filename_without_ext}:{classification}'
+    else:
+        result = f'{filename_without_ext}:Unknown'
+    
+    return Response(result, status=200, mimetype='text/plain')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8000, threaded=True)
